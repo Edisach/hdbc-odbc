48db6da6bb210f7e701c9de5737ec1bd2cb8352a
Edisach <level007@gmail.com>
2014-07-15 11:06:15 +0100

    add imports

--- tests/Utils.hs
+++ tests/Utils.hs
@@ -1,6 +1,11 @@
 module Utils(connectDB, dbTest) where
+
 import Database.HDBC
 import Database.HDBC.ODBC
+import Database.HDBC.ODBC.Statement
+import Database.HDBC.ODBC.ConnectionImpl(Connection(getIconn))
+import Database.HDBC.ODBC.Connection
+
 import Control.Exception
 
 -- Get a connection to the test database
